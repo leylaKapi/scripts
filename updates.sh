b4387248fdaa20bde0c2e09d88b5adb117aa14d5
csidyel <46521788+csidyel@users.noreply.github.com>
2019-01-25 10:52:45 +0100

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -8,10 +8,8 @@ SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
 # Remove existing downloads and binaries so we can start from scratch.
 sudo apt-get -y remove google-chrome-stable
-sudo rm -f ~/selenium-server-standalone-*.jar
 sudo rm -f ~/chromedriver_linux64.zip
 sudo rm -f /usr/local/bin/chromedriver
-sudo rm -f /usr/local/bin/selenium-server-standalone.jar
 
 # Install Chrome.
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
@@ -27,8 +25,3 @@ sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
 
-# Install Selenium.
-wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
-sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
-sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
-sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
