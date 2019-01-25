42ed0249b32bbb406ea072e860f288a7bcc665ee
csidyel <46521788+csidyel@users.noreply.github.com>
2019-01-25 10:32:37 +0100

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -7,11 +7,11 @@ SELENIUM_STANDALONE_VERSION=3.4.0
 SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
 # Remove existing downloads and binaries so we can start from scratch.
-sudo apt-get remove google-chrome-stable
-rm ~/selenium-server-standalone-*.jar
-rm ~/chromedriver_linux64.zip
-sudo rm /usr/local/bin/chromedriver
-sudo rm /usr/local/bin/selenium-server-standalone.jar
+sudo apt-get -y remove google-chrome-stable
+sudo rm -f ~/selenium-server-standalone-*.jar
+sudo rm -f ~/chromedriver_linux64.zip
+sudo rm -f /usr/local/bin/chromedriver
+sudo rm -f /usr/local/bin/selenium-server-standalone.jar
 
 # Install Chrome.
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
@@ -22,7 +22,7 @@ sudo apt-get -y install google-chrome-stable
 # Install ChromeDriver.
 wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
+sudo rm -f ~/chromedriver_linux64.zip
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
